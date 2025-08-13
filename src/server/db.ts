c85e34f88e4f572de4203785dfec6a0608d400a9
triopscmd <mtrabajoraul@gmail.com>
2025-08-13 21:36:54 +0200

    feat: Implement src/server/db.ts to pass tests

--- src/server/db.ts
+++ src/server/db.ts
@@ -2,16 +2,16 @@
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as typeof globalThis & {
-  prisma?: PrismaClient;
+  db?: PrismaClient;
 };
 
-export const prisma =
-  globalForPrisma.prisma ||
+export const db =
+  globalForPrisma.db ||
   new PrismaClient({
     log:
       process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error'],
   });
 
 if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
+  globalForPrisma.db = db;
 }
